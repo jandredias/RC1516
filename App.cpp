618b07cf3fb83e6d9beb715dd813a881f7cce313
Andre Dias <jdias.email@gmail.com>
2015-09-16 21:20:20 +0100

    fork server

--- App.cpp
+++ App.cpp
@@ -3,23 +3,26 @@
 #include <string>
 
 int main(){
+  SocketTCP socket("jdiastk.com", 59000);
+  std::string message;
   try{
     char msg[1024];
-    SocketTCP socket("jdiastk.com", 59000);
     socket.connect();
     std::cin.getline(msg, 1024);
 
-    std::string message(msg);
+    message = msg;
     std::cout << "Sending message" << std::endl;
     socket.write(message);
     std::cout << "Message sent" << std::endl;
     std::cout << "Reading message" << std::endl;
-    //message = socket.read();
+
     //std::cout << "Message Received" << std::endl;
     //std::cout << message << std::endl;
-    socket.disconnect();
+
   }catch(std::string s){
     std::cout << s << std::endl;
   }
+  message = socket.read();
+  socket.disconnect();
   return 0;
 }
