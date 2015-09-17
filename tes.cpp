72e0dd921ba1ec99e13f44aecb65b04f7f66d207
Miguel Carvalho <miguel.pokemon@gmail.com>
2015-09-17 21:29:29 +0100

    Fixed the argc IF shit, it was not accepting the right number of parameters.

--- tes.cpp
+++ tes.cpp
@@ -1,9 +1,14 @@
+#include "SocketTCP.h"
 #include <iostream>
+#include <string>
+#include <cstdlib>
+
 #define __PORT__ 58023
 
 int main(int argc, char* argv[]){
+  std::cout << argc<<std::endl;
   int port = __PORT__;
-  if(argc != 1 || argc != 3){ std::cout <<"wrong number of parameteres" << std::endl; return 1; }
+  if(!(argc == 1 || argc == 3)){ std::cout <<"wrong number of parameteres" << std::endl; return 1; }
   for(int i = 1; i < argc; i += 2){  
     if(std::string(argv[i]) != std::string("-p")){
       std::cout << "wrong format of parameters" << std::endl;
